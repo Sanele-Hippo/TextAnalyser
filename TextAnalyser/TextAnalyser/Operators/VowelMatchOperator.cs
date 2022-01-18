9b0b94600f5289f4da044bc5f7f79f14bf1854db
Sanele Mvubu <maguludla@gmail.com>
2022-01-19 00:00:56 +0200

    Add Match Operator and Unit test

--- TextAnalyser/TextAnalyser/Operators/VowelMatchOperator.cs
+++ TextAnalyser/TextAnalyser/Operators/VowelMatchOperator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextAnalyser.Extensions;
 
 namespace TextAnalyser.Operators
 {
@@ -10,7 +11,13 @@ namespace TextAnalyser.Operators
   {
     public string ExecuteOperation(string input)
     {
-      throw new NotImplementedException();
+      var vowelCount = base.GetVowelCount(input);
+      var nonVowels = input.Clean().Length - vowelCount;
+
+      if (vowelCount == nonVowels)
+        return "The text has an equal amount of vowels and non vowels";
+
+      return nonVowels > vowelCount ? "The text has more non vowels than vowels" : "The text has more vowels than non vowels";
     }
    }
 }
---
+++ TextAnalyser/TextAnalyserTest/Operators/VowelMatchOperatorTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextAnalyser;
+
+namespace TextAnalyserTest
+{
+  public class VowelMatchOperatorTest
+  {
+    private readonly int _matchOperatorId = 3;
+
+    [SetUp]
+    public void SetUp() { }
+
+    [TearDown]
+    public void TearDown() { }
+
+    [TestCase("I eat", "The text has more vowels than non vowels")]
+    [TestCase("that dog", "The text has more non vowels than vowels")]
+    [TestCase("3 a", "The text has an equal amount of vowels and non vowels")]
+    [TestCase("", "The text has an equal amount of vowels and non vowels")]
+    public void Test_VowelMatchOperator_Should_Return_Success(string input, string result)
+    {
+      //Arrange
+      var op = OperatorFactory.Create(_matchOperatorId);
+
+      //Act
+      var response = op.ExecuteOperation(input);
+
+      //Assert
+      Assert.AreEqual(response, result);
+    }
+  }
+}
