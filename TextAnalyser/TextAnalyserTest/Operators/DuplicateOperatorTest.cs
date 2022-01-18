0341bd1c455cfdb9dad465459908e3dca31113e7
Sanele Mvubu <maguludla@gmail.com>
2022-01-19 00:05:29 +0200

    renaming test method

--- TextAnalyser/TextAnalyserTest/Operators/DuplicateOperatorTest.cs
+++ TextAnalyser/TextAnalyserTest/Operators/DuplicateOperatorTest.cs
@@ -20,7 +20,7 @@ namespace TextAnalyserTest
 
     [TestCase("I like eating apples", "Found the following duplicates: ileap")]
     [TestCase("ApPles", "Found the following duplicates: p")]
-    [TestCase("like", "No duplicate values were found")]
+    [TestCase("abcd4", "No duplicate values were found")]
     [TestCase("", "No duplicate values were found")]
     public void Test_DuplicateOperator_Should_Return_Success(string input, string result)
     {
